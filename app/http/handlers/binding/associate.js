547aa9df07bd4345068d6463d199f7242cc57089
Jared Hanson <jaredhanson@gmail.com>
2017-07-02 15:22:09 -0700

    Return association params.

--- app/http/handlers/binding/associate.js
+++ app/http/handlers/binding/associate.js
@@ -1,19 +1,48 @@
-exports = module.exports = function(parse, mfaRegister) {
+exports = module.exports = function(parse, mfaAssociate) {
+  
+  function restoreContext(req, res, next) {
+    req.user = { id: '1' }
+    next();
+    
+    // FIXME: Put this back
+    /*
+    Tokens.decipher(req.body.mfa_token, function(err, claims, issuer) {
+      if (err) { return next(err); }
+      
+      authenticateToken(claims.subject, issuer, function(err, user) {
+        if (err) { return next(err); }
+        // TODO: 404, if no user
+        
+        req.client = req.user;
+        req.user = user;
+        next();
+      });
+    });
+    */
+  }
   
   function respond(req, res, next) {
     console.log('REGISTERING FOR MFA???');
     console.log(req.headers)
     console.log(req.body)
+    console.log(req.user)
     
-    mfaRegister(function(err, out) {
+    mfaAssociate(req.user, function(err, params) {
       
+      var body = {};
+      body.authenticator_type = 'oob';
+      body.secret = params.secret;
+      body.url = params.url;
       
+      return res.json(body);
     });
   }
 
 
   return [
     parse('application/json'),
+    // TODO: Authenticate OAuth client
+    restoreContext,
     respond
   ];
   
@@ -21,5 +50,5 @@ exports = module.exports = function(parse, mfaRegister) {
 
 exports['@require'] = [
   'http://i.bixbyjs.org/http/middleware/parse',
-  'http://schemas.authnomicon.org/js/login/mfa/opt/auth0/register'
+  'http://schemas.authnomicon.org/js/login/mfa/opt/auth0/associate'
 ];
