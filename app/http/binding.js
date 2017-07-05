67ecfe6487d9114f3e6eb1213c0a96fc3074f3ba
Jared Hanson <jaredhanson@gmail.com>
2017-07-04 17:10:53 -0700

    Remove bind endpoint, for now.

--- app/http/binding.js
+++ app/http/binding.js
@@ -1,15 +1,13 @@
-exports = module.exports = function(associateHandler, bindHandler) {
+exports = module.exports = function(associateHandler) {
   var express = require('express');
   var router = new express.Router();
   
   router.post('/associate', associateHandler);
-  //router.post('/bind', bindHandler);
   
   return router;
 };
 
 exports['@implements'] = 'http://schemas.authnomicon.org/js/http/oauth2/mfa/BindingService';
 exports['@require'] = [
-  './handlers/binding/associate',
-  './handlers/binding/bind'
+  './handlers/binding/associate'
 ];
