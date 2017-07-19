9e0f483bb30f4a3b646ecfd467da9e0318ceff82
Jared Hanson <jaredhanson@gmail.com>
2017-07-18 19:41:37 -0700

    Update tests.

--- test/package.test.js
+++ test/package.test.js
@@ -12,7 +12,12 @@ describe('@authnomicon/oauth2-mfa', function() {
       expect(json.assembly.namespace).to.equal('oauth2/mfa');
       
       expect(json.assembly.components).to.have.length(8);
+      expect(json.assembly.components).to.include('oob/authenticator');
       expect(json.assembly.components).to.include('oob/grant');
+      expect(json.assembly.components).to.include('otp/grant');
+      expect(json.assembly.components).to.include('recovery/grant');
+      expect(json.assembly.components).to.include('tokens/oob-code/interpret');
+      expect(json.assembly.components).to.include('tokens/oob-code/translate');
     });
   });
   
