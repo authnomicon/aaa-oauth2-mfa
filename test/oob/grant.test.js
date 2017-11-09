0bb71009eb4f8704c04d6bac48889408bd2579cb
Jared Hanson <jaredhanson@gmail.com>
2017-11-09 16:29:00 +0100

    Add tests.

--- test/oob/grant.test.js
+++ test/oob/grant.test.js
@@ -1,5 +1,6 @@
 /* global describe, it */
 
+var $require = require('proxyquire');
 var expect = require('chai').expect;
 var sinon = require('sinon');
 var factory = require('../../app/oob/grant');
@@ -14,6 +15,21 @@ describe('oob/grant', function() {
   it('should be annotated', function() {
     expect(factory['@implements']).to.equal('http://schemas.authnomicon.org/js/oauth2/grantType');
     expect(factory['@type']).to.equal('http://auth0.com/oauth/grant-type/mfa-oob');
+  });
+  
+  describe('creating grant', function() {
+    var oobSpy = sinon.stub();
+    var issue = function(){};
+    var authenticate = function(){};
+    
+    var factory = $require('../../app/oob/grant',
+      { 'oauth2orize-mfa': { exchange: { oob: oobSpy } } });
+    var exchange = factory(issue, authenticate);
+    
+    it('should create exchange', function() {
+      expect(oobSpy).to.have.been.calledOnce;
+      expect(oobSpy).to.have.been.calledWithExactly({ passReqToCallback: true }, authenticate, issue);
+    });
   });
   
 });
